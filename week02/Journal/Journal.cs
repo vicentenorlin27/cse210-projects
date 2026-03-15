3ce0efd0e6a30427ab79e372f8a4200c5a92aeeb
norlinvicente2798 <norlin.vicente2798@gmail.com>
2026-03-15 17:08:06 -0400

    did the journal and created all files required

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+        _entries.Add(newEntry);
+    }
+
+    public void DisplayAll()
+    {
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+
+    public void SaveToFile(string fileName)
+    {
+        using (StreamWriter outputFile = new StreamWriter(fileName))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine(entry.ToFileString());
+            }
+        }
+    }
+
+    public void LoadFromFile(string fileName)
+    {
+        string[] lines = File.ReadAllLines(fileName);
+
+        _entries.Clear();
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("~|~");
+
+            Entry entry = new Entry();
+            entry._date = parts[0];
+            entry._promptText = parts[1];
+            entry._responseText = parts[2];
+            entry._mood = parts[3];
+
+            _entries.Add(entry);
+        }
+    }
+}
